6adfa0d2c64f78e7b6dd998dba6f81321d24a242
iprankurpandey <pprankur@gmail.com>
2022-04-12 11:19:30 +0530

    implemented video controls

--- video-app/src/backend/db/videos.js
+++ video-app/src/backend/db/videos.js
@@ -13,7 +13,7 @@ export const videos = [
     catagory: "Technology",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/7vQznLKEfBs/mqdefault_6s.webp?du=3000&sqp=CO7r05IG&rs=AOn4CLCUYOL2XfPA92EDnvR5zlBpBfxwPw",
-    videoUrl: "https://www.youtube.com/watch?v=rQFZdcPphwU",
+    videoUrl: "https://www.youtube.com/embed/rQFZdcPphwU",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLSR1kOOGcsmya2i1HCjJUYDwtN6XCGYMEJYD4HL=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -25,7 +25,7 @@ export const videos = [
     catagory: "Technology",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/Oa9aWdcCC4o/mqdefault_6s.webp?du=3000&sqp=CJTS05IG&rs=AOn4CLCg19nv7-86uWJQnubxJtTjwzT-RQ",
-    videoUrl: "https://www.youtube.com/watch?v=EJi3Vxdyqv0",
+    videoUrl: "https://www.youtube.com/embed/EJi3Vxdyqv0",
     creator_pic:
       "https://yt3.ggpht.com/7BZtp1qb_yK4JhwqAc6urnssMYJR98f8274pFh5UKa3_wrzG3Qsmy_QieQO_iV0ac4X8_CIJ3Yw=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -37,7 +37,7 @@ export const videos = [
     catagory: "Technology",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/JoTtD3ZSueU/mqdefault_6s.webp?du=3000&sqp=CIDc05IG&rs=AOn4CLCk2y9y4sDLQZ6CIMVavpJRPsoG3A",
-    videoUrl: "https://www.youtube.com/watch?v=ldwr0HXpDSo",
+    videoUrl: "https://www.youtube.com/embed/ldwr0HXpDSo",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLQh0YhlrGYPYpZemAGl__vrKCsRQxCOXplFDgcC_Q=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -48,7 +48,7 @@ export const videos = [
     catagory: "Technology",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/iGkl34KTRaU/mqdefault_6s.webp?du=3000&sqp=CPjr05IG&rs=AOn4CLAAdNH9tAD8MK9MmF7UFy6BYoxUFg",
-    videoUrl: "https://www.youtube.com/watch?v=_jdPC5F83Fo",
+    videoUrl: "https://www.youtube.com/embed/_jdPC5F83Fo",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLQ5DCS55gDHcjd5NcL0Rjvu5ghRopQo-zKP5giPDg=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -60,7 +60,7 @@ export const videos = [
     catagory: "Comedy",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/Xx_YoErvJo4/mqdefault_6s.webp?du=3000&sqp=COrM05IG&rs=AOn4CLA2eX_N6_7RGnJM7Jq4_MNw7QcaLA",
-    videoUrl: "https://www.youtube.com/watch?v=Xx_YoErvJo4",
+    videoUrl: "https://www.youtube.com/embed/Xx_YoErvJo4",
     creator_pic:
       "https://yt3.ggpht.com/kFIm8K_VJqJKFBt8ePn-wVIvG0ZYeIQxw7Ok9eEkYjOSsyebG7wStrOQOcv4GDtbd6Y4zZa9oo0=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -71,7 +71,7 @@ export const videos = [
     catagory: "Comedy",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/Nj4H-X3FAWU/mqdefault_6s.webp?du=3000&sqp=CIHi05IG&rs=AOn4CLBqrNcsLHsjSqccZVwpiZ8VJN252w",
-    videoUrl: "https://www.youtube.com/watch?v=Nj4H-X3FAWU",
+    videoUrl: "https://www.youtube.com/embed/Nj4H-X3FAWU",
     creator_pic:
       "https://yt3.ggpht.com/DzaZaTX6gdgjjPI_vkNc2dPbI794UroI9fTAunua0fa7lukDj5NDkjfhS5-w2KXuvXS02r92=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -82,7 +82,7 @@ export const videos = [
     catagory: "Comedy",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/Xx_YoErvJo4/mqdefault_6s.webp?du=3000&sqp=COrM05IG&rs=AOn4CLA2eX_N6_7RGnJM7Jq4_MNw7QcaLA",
-    videoUrl: "https://www.youtube.com/watch?v=TTLcqOm-3vU",
+    videoUrl: "https://www.youtube.com/embed/TTLcqOm-3vU",
     creator_pic:
       "https://yt3.ggpht.com/2-ooILRw_I--NvYcpH4a6lIn3WC3DFXLR1a8XsnXezw1QcZr3uc5itjdKTNsQsi09tNm04lWRFI=s48-c-k-c0x00ffffff-no-rj",
   },
@@ -93,7 +93,7 @@ export const videos = [
     catagory: "Comedy",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/A8h7Atk5sX4/mqdefault_6s.webp?du=3000&sqp=CLz605IG&rs=AOn4CLBcJxEIXsZs-btSt0_LjD346Ql4Nw",
-    videoUrl: "https://www.youtube.com/watch?v=KBW615WWUsA",
+    videoUrl: "https://www.youtube.com/embed/KBW615WWUsA",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLTomWcdvRB8foCRDX-ft_sqt3U6NCtf_uLaa9-wTzY=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -104,7 +104,7 @@ export const videos = [
     catagory: "Coding",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/eKqY-oP1d_Y/mqdefault_6s.webp?du=3000&sqp=COLl05IG&rs=AOn4CLAKU0XbQa4o4qeoMBaV1WoVtonaFA",
-    videoUrl: "https://www.youtube.com/watch?v=eKqY-oP1d_Y",
+    videoUrl: "https://www.youtube.com/embed/eKqY-oP1d_Y",
     creator_pic:
       "https://yt3.ggpht.com/O12gYmCwBASezJpxddXOj1PEirMgxCGX2gOiJ3plomaK4E0K1hr_iobbQEWz1e4QVMflTmug=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -115,7 +115,7 @@ export const videos = [
     catagory: "Coding",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/tv_9doDQJU0/mqdefault_6s.webp?du=3000&sqp=CIPX05IG&rs=AOn4CLD2A8k-HglbYmJhNouV6pUzIPZ-ig",
-    videoUrl: "https://www.youtube.com/watch?v=tv_9doDQJU0",
+    videoUrl: "https://www.youtube.com/embed/tv_9doDQJU0",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLTaNnIZrEVNHk-OgfTRlO1n2aeAZ657f5FrG-5EaA=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -126,7 +126,7 @@ export const videos = [
     catagory: "Coding",
     thumbnailUrl:
       "https://i.ytimg.com/vi/Xm6z0_Lb7L0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBU7LgDmmTVEP_KniY89YOxbY6cdg",
-    videoUrl: "https://www.youtube.com/watch?v=Xm6z0_Lb7L0&t=1035s",
+    videoUrl: "https://www.youtube.com/embed/Xm6z0_Lb7L0&t=1035s",
     creator_pic:
       "https://yt3.ggpht.com/nL8PiMlQHO3250kGuUJ9EdXN7Ha9c-0qTxptczBSKqn6KMsK-3UBEQSz4r2K7Hy-rxkwsiiepA=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -137,7 +137,7 @@ export const videos = [
     catagory: "Coding",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/-KbuVuVDzCc/mqdefault_6s.webp?du=3000&sqp=CITu05IG&rs=AOn4CLAN7iYdv-tCoL5aYFNqJo6xSgCVQw",
-    videoUrl: "https://www.youtube.com/watch?v=-KbuVuVDzCc",
+    videoUrl: "https://www.youtube.com/embed/-KbuVuVDzCc",
     creator_pic:
       "https://yt3.ggpht.com/j_ZCn6PJZufZpCMRI9zRxWxnGkPY_PY5erbypulqeRPyPA2jZhGaTVVgyJH5NI2KPkKQRqeEneg=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -149,7 +149,7 @@ export const videos = [
     catagory: "Coding",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/0OQtLP3Q-PE/mqdefault_6s.webp?du=3000&sqp=CIDm05IG&rs=AOn4CLA4YTgZF1XcNBEfGK-tQzsUCjYykA",
-    videoUrl: "https://www.youtube.com/watch?v=0OQtLP3Q-PE",
+    videoUrl: "https://www.youtube.com/embed/0OQtLP3Q-PE",
     creator_pic:
       "https://yt3.ggpht.com/j_ZCn6PJZufZpCMRI9zRxWxnGkPY_PY5erbypulqeRPyPA2jZhGaTVVgyJH5NI2KPkKQRqeEneg=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -160,7 +160,7 @@ export const videos = [
     catagory: "Tedx",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/H20GYBypdRM/mqdefault_6s.webp?du=3000&sqp=COXw05IG&rs=AOn4CLDXiW32ZS82hBa64kcERZX-cDV-JQ",
-    videoUrl: "https://www.youtube.com/watch?v=H20GYBypdRM",
+    videoUrl: "https://www.youtube.com/embed/H20GYBypdRM",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLSAltH3yarKh0eytWLSLtB9-kPpMLM5EejH-GAZIg=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -171,7 +171,7 @@ export const videos = [
     catagory: "Tedx",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/Hu4Yvq-g7_Y/mqdefault_6s.webp?du=3000&sqp=CKzY05IG&rs=AOn4CLA41ze0rJaneTopfsKh8NHDNBkmMg",
-    videoUrl: "https://www.youtube.com/watch?v=Hu4Yvq-g7_Y",
+    videoUrl: "https://www.youtube.com/embend/Hu4Yvq-g7_Y",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLSAltH3yarKh0eytWLSLtB9-kPpMLM5EejH-GAZIg=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -182,7 +182,7 @@ export const videos = [
     catagory: "Tedx",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/IgKWPdJWuBQ/mqdefault_6s.webp?du=3000&sqp=CMzx05IG&rs=AOn4CLCV5H2K2EOv2SEeMSQIdp2il-qP4w",
-    videoUrl: "https://www.youtube.com/watch?v=IgKWPdJWuBQ",
+    videoUrl: "https://www.youtube.com/embed/IgKWPdJWuBQ",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLSAltH3yarKh0eytWLSLtB9-kPpMLM5EejH-GAZIg=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -193,7 +193,7 @@ export const videos = [
     catagory: "Tedx",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/4AzpmZ7AjaQ/mqdefault_6s.webp?du=3000&sqp=COzi05IG&rs=AOn4CLDJHTayBlKjywvUE-ai8g3Kk6_A0Q",
-    videoUrl: "https://www.youtube.com/watch?v=4AzpmZ7AjaQ",
+    videoUrl: "https://www.youtube.com/embed/4AzpmZ7AjaQ",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLSAltH3yarKh0eytWLSLtB9-kPpMLM5EejH-GAZIg=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -204,7 +204,7 @@ export const videos = [
     catagory: "Technology",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/M988_fsOSWo/mqdefault_6s.webp?du=3000&sqp=CP7Y05IG&rs=AOn4CLA7-pM1BQfacMmDVu1IuG1lcxP8oA",
-    videoUrl: "https://www.youtube.com/watch?v=M988_fsOSWo",
+    videoUrl: "https://www.youtube.com/embed/M988_fsOSWo",
     creator_pic:
       "https://yt3.ggpht.com/ytc/AKedOLSAltH3yarKh0eytWLSLtB9-kPpMLM5EejH-GAZIg=s68-c-k-c0x00ffffff-no-rj",
   },
@@ -215,7 +215,7 @@ export const videos = [
     catagory: "Technology",
     thumbnailUrl:
       "https://i.ytimg.com/an_webp/EJi3Vxdyqv0/mqdefault_6s.webp?du=3000&sqp=CPvR05IG&rs=AOn4CLC4wwVTrKsXgSTMRsb_D2c_JCPHEw",
-    videoUrl: "https://www.youtube.com/watch?v=EJi3Vxdyqv0",
+    videoUrl: "https://www.youtube.com/embed/EJi3Vxdyqv0",
     creator_pic:
       "https://yt3.ggpht.com/j_ZCn6PJZufZpCMRI9zRxWxnGkPY_PY5erbypulqeRPyPA2jZhGaTVVgyJH5NI2KPkKQRqeEneg=s68-c-k-c0x00ffffff-no-rj",
   },
--- video-app/src/component/Videocard.js
+++ video-app/src/component/Videocard.js
@@ -1,16 +1,28 @@
 import React from "react";
 import "./Videocard.css";
 function Videocard({ video }) {
-  const { title, catagory, thumbnailUrl, creator_pic, videoUrl } = video;
-  console.log(video);
-
+  const { id, title, catagory, thumbnailUrl, creator_pic, videoUrl } = video;
+  const url = "www.youtube.com/embed/0KNk-Joi-NM?list=RD0KNk-Joi-NM";
+  console.table(videoUrl);
   return (
     <div>
       <div className="video-card-item">
         <div className="video-card">
           <div className="video-card-image">
+            <div class="box1">Hover over me.</div>
+            <div class="box2">
+              I am shown when someone hovers over the div above.
+              <iframe
+                width="350"
+                height="240"
+                src={videoUrl}
+                // src="https://www.youtube.com/embed/tgbNymZ7vqY"
+                title="band bajao"
+              ></iframe>
+            </div>
             <img src={thumbnailUrl} alt="react" />
           </div>
+
           <div className="video-card-details">
             <div className="video-card-logo">
               <img src={creator_pic} alt="logo" />
