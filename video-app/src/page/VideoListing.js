a45db9e4fd7e51e606bb1d29ce85e215304fa488
iprankurpandey <pprankur@gmail.com>
2022-04-12 10:06:33 +0530

    fix : video listing

--- video-app/src/page/VideoListing.js
+++ video-app/src/page/VideoListing.js
@@ -2,12 +2,15 @@ import React from "react";
 import Footer from "../component/Footer";
 import Header from "../component/Header";
 import Sidebar from "../component/Sidebar";
-
+import { Videocard } from "../component/Videocard";
+import Videodata from "../component/Videodata";
+import "../component/Videocard.css";
 function VideoListing() {
   return (
     <div>
       <Header />
       <Sidebar />
+      <Videodata />
       <Footer />
     </div>
   );
