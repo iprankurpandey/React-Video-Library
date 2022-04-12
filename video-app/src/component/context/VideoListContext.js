3591b787f3cc7c710d28f36509befc7c352a94f0
iprankurpandey <pprankur@gmail.com>
2022-04-12 10:06:21 +0530

    added context

---
+++ video-app/src/component/context/VideoListContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+export const VideoListingContext = createContext();
+
+function VideoListContext({ children }) {
+  const [videoList, setVideoList] = useState([]);
+  return (
+    <div>
+      <VideoListingContext.Provider value={{ videoList, setVideoList }}>
+        {children}
+      </VideoListingContext.Provider>
+    </div>
+  );
+}
+
+export { VideoListContext };
