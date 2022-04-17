0b875d8b081e02875431798eb71ab8e18a4aeca7
iprankurpandey <pprankur@gmail.com>
2022-04-17 15:25:28 +0530

    feat : posted api calls for playlists

---
+++ video-app/src/component/Model.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Model.css";
+function Model() {
+  return (
+    <div>
+      <div className="model">
+        <span class="material-icons">add</span>
+        ADD TO PLAYLIST
+      </div>
+    </div>
+  );
+}
+
+export default Model;
